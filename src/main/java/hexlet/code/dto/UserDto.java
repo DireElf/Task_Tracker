f0555800e659331197bcf97c6670b03679d4aca8
DireElf <3672@mail.ru>
2022-11-17 13:23:50 +0700

    fields validation added

--- src/main/java/hexlet/code/dto/UserDto.java
+++ src/main/java/hexlet/code/dto/UserDto.java
@@ -1,19 +1,25 @@
 package hexlet.code.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public final class UserDto {
     @NotBlank
+    @Email(message = "Incorrect email format")
     private String email;
 
     @NotBlank
+    @Size(min = 1, message = "First name must be longer than 1 character")
     private String firstName;
 
     @NotBlank
+    @Size(min = 1, message = "Last name must be longer than 1 character")
     private String lastName;
 
     @NotBlank
+    @Size(min = 3, message = "Password must be longer than 3 characters")
     private String password;
 
     // constructors
