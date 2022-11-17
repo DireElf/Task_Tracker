583cb078276e7bd6bfc38e89f6134cbce0a4f949
DireElf <3672@mail.ru>
2022-11-17 10:32:40 +0700

    request mapping added

--- src/main/java/hexlet/code/controllers/WelcomeController.java
+++ src/main/java/hexlet/code/controllers/WelcomeController.java
@@ -1,9 +1,11 @@
 package hexlet.code.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("${base-url}")
 public final class WelcomeController {
 
     @GetMapping("/welcome")
