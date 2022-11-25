fb24824793085f01677c853c2fb1555c30a33ef0
DireElf <direelf@Main.localdomain>
2022-11-25 15:28:00 +0700

    rollbar integration added

--- src/main/java/hexlet/code/controller/UserController.java
+++ src/main/java/hexlet/code/controller/UserController.java
@@ -1,5 +1,6 @@
 package hexlet.code.controller;
 
+import com.rollbar.notifier.Rollbar;
 import hexlet.code.dto.UserDto;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
@@ -39,6 +40,7 @@ public class UserController {
 
     private final UserService userService;
     private final UserRepository userRepository;
+    private final Rollbar rollbar;
 
     private static final String ONLY_OWNER_BY_ID = """
                 @userRepository.findById(#id).get().getEmail() == authentication.getName()
@@ -57,6 +59,7 @@ public class UserController {
         ))
     @GetMapping("")
     public List<User> getAllUsers() throws Exception {
+        rollbar.debug("Test message");
         return userRepository.findAll()
                 .stream()
                 .toList();
